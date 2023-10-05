55f5fc04af7d627fd637788555fa52d2bd8e0136
Zeynep Aydin <78844470+z1almagul@users.noreply.github.com>
2023-10-05 22:25:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener('load',function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = 1000;
-    canvas.height = 800;
+    canvas.height = 1000;
 
     class InputHandler{
         constructor(game){
