bfccbff635bc6a5fb5b1e2a24f5dd135fe54c2f2
Zeynep Aydin <78844470+z1almagul@users.noreply.github.com>
2023-10-05 22:12:34 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(){
+  //canvas setup
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getcontext('2d');
+  canvas.width = 1000;
+  canvas.height = 1000;
+});
